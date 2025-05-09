0058f65291ad376f3fc973caecf1d52046c7d09f
ea7hie <ea7hie@mail.ru>
2025-05-09 22:29:13 +0300

    Созданы сущности и исключения, их дто. Спринт 14, Ревью 1, Версия проекта 1

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,29 @@
 package ru.practicum.shareit.booking;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+
+@Data
+@AllArgsConstructor
 public class Booking {
+    private long id;
+
+    @FutureOrPresent
+    private LocalDateTime start;
+
+    @FutureOrPresent
+    private LocalDateTime end;
+
+    private Item item;
+    private User booker;
+    private BookingStatus status;
 }
---
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.user.dto;
+
+import ru.practicum.shareit.user.User;
+
+public class UserMapper {
+    public static UserDto toUserDto(User user) {
+        return new UserDto(
+                user.getName()
+        );
+    }
+}
