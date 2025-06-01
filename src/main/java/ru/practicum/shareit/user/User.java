d5bd52b15e2b0ce7f1a212792039637325542403
ea7hie <ea7hie@mail.ru>
2025-06-01 22:01:54 +0300

    Изменена сущность User в соответсвии с JPA. Спринт 15, Ревью 1, Версия проекта 2

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,5 +1,9 @@
 package ru.practicum.shareit.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -11,12 +15,17 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
     private long id;
 
     @NotBlank(message = "Имя пользователя не может быть пустым")
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Email(message = "Email пользователя не соответствует формату")
+    @Column(name = "email", nullable = false)
     private String email;
 }
