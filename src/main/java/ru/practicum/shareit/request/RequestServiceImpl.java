60c5583f35f171daa88664322ec0ec7b0f7d09f6
ea7hie <ea7hie@mail.ru>
2025-06-04 17:21:09 +0300

    Полностью обновлена в соответствии с jpa логика по работе с сущностью ItemRequest. Спринт 15, Ревью 1, Версия проекта 2

--- src/main/java/ru/practicum/shareit/request/RequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/RequestServiceImpl.java
@@ -2,6 +2,9 @@ package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.enums.Actions;
+import ru.practicum.shareit.exception.model.AccessError;
+import ru.practicum.shareit.request.dao.RequestChecks;
 import ru.practicum.shareit.request.dao.RequestRepository;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestMapper;
@@ -16,44 +19,62 @@ public class RequestServiceImpl implements RequestService {
     private final RequestRepository requestRepository;
     private final UserRepository userRepository;
 
+    private final String messageCantUpdate = "У вас нет прав доступа к редактированию этого запроса.";
+    private final String messageCantDelete = "У вас нет прав доступа к удалению этого запроса.";
+
     @Override
     public ItemRequestDto createItemRequest(ItemRequestDto itemRequestDto, long userId) {
+        UserChecks.isUserExistsById(userRepository, userId);
         ItemRequest itemRequest = ItemRequestMapper.toItemRequest(itemRequestDto);
-        return ItemRequestMapper.toItemRequestDto(requestRepository.createItemRequest(itemRequest));
+        return ItemRequestMapper.toItemRequestDto(requestRepository.save(itemRequest));
     }
 
     @Override
     public Collection<ItemRequestDto> getAllItemRequest() {
-        return requestRepository.getAllItemRequest().stream()
+        return requestRepository.findAll().stream()
                 .map(ItemRequestMapper::toItemRequestDto)
                 .toList();
     }
 
     @Override
     public ItemRequestDto getItemRequestById(long itemRequestId) {
-        return ItemRequestMapper.toItemRequestDto(requestRepository.getItemRequestById(itemRequestId));
+        return ItemRequestMapper.toItemRequestDto(RequestChecks.getItemRequestOrThrow(requestRepository,
+                itemRequestId, Actions.TO_VIEW));
 
     }
 
     @Override
     public Collection<ItemRequestDto> getAllItemRequestsFromRequester(long requesterId) {
         UserChecks.isUserExistsById(userRepository, requesterId);
-        return requestRepository.getAllItemRequestsFromRequester(requesterId).stream()
+        return requestRepository.findAllByRequesterId(requesterId).stream()
                 .map(ItemRequestMapper::toItemRequestDto)
                 .toList();
     }
 
     @Override
     public ItemRequestDto updateItemRequest(ItemRequestDto itemRequestDtoForUpdate, long userId, long itemReqId) {
-        UserChecks.isUserExistsById(userRepository, userId);
-        itemRequestDtoForUpdate.setId(itemReqId);
-        return ItemRequestMapper.toItemRequestDto(requestRepository.updateItemRequest(itemRequestDtoForUpdate, userId));
+        ItemRequest itemReqForUpdate = RequestChecks.getItemRequestOrThrow(requestRepository,
+                itemReqId, Actions.TO_UPDATE);
+
+        if (itemReqForUpdate.getRequesterId() == userId) {
+            itemReqForUpdate.setDescription(itemRequestDtoForUpdate.getDescription() == null ?
+                    itemReqForUpdate.getDescription() : itemRequestDtoForUpdate.getDescription());
+
+            requestRepository.updateItemRequest(itemReqId, itemReqForUpdate.getDescription());
+            return ItemRequestMapper.toItemRequestDto(itemReqForUpdate);
+        }
+        throw new AccessError(messageCantUpdate);
     }
 
     @Override
     public ItemRequestDto deleteItemRequest(long itemRequestIdForDelete, long userId) {
-        UserChecks.isUserExistsById(userRepository, userId);
-        return ItemRequestMapper.toItemRequestDto(requestRepository.deleteItemRequest(itemRequestIdForDelete, userId));
+        ItemRequest itemRequestForDelete = RequestChecks.getItemRequestOrThrow(requestRepository,
+                itemRequestIdForDelete, Actions.TO_DELETE);
 
+        if (itemRequestForDelete.getRequesterId() == userId) {
+            requestRepository.deleteById(itemRequestIdForDelete);
+            return ItemRequestMapper.toItemRequestDto(itemRequestForDelete);
+        }
+        throw new AccessError(messageCantDelete);
     }
 }
